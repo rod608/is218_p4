51fa919fdcf583111a404a417fa8476913c51042
rod608 <rod608ney@gmail.com>
2022-05-11 10:59:38 -0400

    Optimized some imports

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,20 +1,16 @@
-import logging
-
 # Project 3: Fix mail, Check out that pagination section
 
-from flask import Blueprint, render_template, redirect, url_for, flash, current_app, abort
+from flask import Blueprint, render_template, redirect, url_for, flash, abort
 from flask_login import login_user, login_required, logout_user, current_user
+from flask_mail import Message
 from jinja2 import TemplateNotFound
-from sqlalchemy import select
 from werkzeug.security import generate_password_hash
 
 from app.auth.decorators import admin_required
 from app.auth.forms import login_form, register_form, profile_form, security_form, user_edit_form, create_user_form
-from app.db import db
-from app.db.models import User, Location, location_user
-from flask_mail import Message
-
 from app.auth.user_management import user_management
+from app.db import db
+from app.db.models import User, Location
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 auth.register_blueprint(user_management, url_prefix="")
